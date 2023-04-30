863307443da126df2748853d660fe4ed5a742e0a
Nnennia <nnenniaatuanya@gmail.com>
2023-04-30 08:24:03 +0100

    commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+"""float sum function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """add function
+    """
+    return (a + b)
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import math
+"""Write a type-annotated function floor
+which takes a float n as
+argument and returns the floor of the float."""
+
+
+def floor(n: float) -> float:
+    return math.floor(n)
